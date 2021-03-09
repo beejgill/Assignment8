d2f27c68f9d86b1666cf55472618f6ba657f6916
beejgill <71659662+beejgill@users.noreply.github.com>
2021-03-09 09:18:56 -0700

    Add files via upload

---
+++ Models/Cart.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models
+{
+    public class Cart
+    {
+        public List<CartLine> Lines { get; set; } = new List<CartLine>();
+
+        // create new CartLine object or update quantity on existing object
+        public virtual void AddItem (Book book, int quantity)
+        {
+            CartLine line = Lines.Where(b => b.Book.BookId == book.BookId).FirstOrDefault();
+
+            if (line == null)
+            {
+                Lines.Add(new CartLine
+                {
+                    Book = book,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public virtual void RemoveLine(Book book) =>
+            Lines.RemoveAll(x => x.Book.BookId == book.BookId);
+
+        public virtual void Clear() => Lines.Clear();
+
+        public float ComputeTotalSum() => Lines.Sum(e => e.Book.Price * e.Quantity);
+
+        public class CartLine
+        {
+            public int CartLineID { get; set; }
+            public Book Book { get; set; }
+            public int Quantity { get; set; }
+        }
+    }
+}
---
+++ Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models.ViewModels
+{
+    public class PagingInfo
+    {
+        public int TotalNumItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        // calculates the total number of pages needed based on the number of items and the number of items we want on each page
+        public int TotalPages => (int)(Math.Ceiling((decimal) TotalNumItems / ItemsPerPage));
+    }
+}
