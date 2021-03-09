6c11c14cdfc7a8e2b1fb3791c1c5c65d87ec9979
beejgill <71659662+beejgill@users.noreply.github.com>
2021-03-09 09:10:17 -0700

    Add files via upload

---
+++ Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Assignment5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IBookRepository repository;
+
+        // constructor
+        public NavigationMenuViewComponent (IBookRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            // sets the selected category in the ViewBag
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+
+            // returns the list of distinct categories to the NavigationMenu Default view
+            return View(repository.Books
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
